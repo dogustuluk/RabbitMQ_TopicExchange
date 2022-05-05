57ce922dff108beb3cac5a477e3b7871a18918a5
dogustuluk <dogus.tuluk@gmail.com>
2022-05-05 16:04:29 +0300

    subscriber2 topic exchange

--- RabbitMQ.subscriber2/Program.cs
+++ RabbitMQ.subscriber2/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
 
 namespace RabbitMQ.subscriber2
 {
@@ -6,7 +10,42 @@ namespace RabbitMQ.subscriber2
     {
         static void Main(string[] args)
         {
-            
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://nxdranwu:n_3nr-xZlXx0NoCWuFP05gTqZfp7_hwK@sparrow.rmq.cloudamqp.com/nxdranwu");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("logs-topic", durable: true, type: ExchangeType.Topic);
+
+            channel.BasicQos(0, 1, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            var queueName = channel.QueueDeclare().QueueName;
+
+            var routeKey = "Info.#";
+
+            channel.QueueBind(queueName, "logs-topic", routeKey);
+
+            channel.BasicConsume(queueName, false, consumer);
+
+            Console.WriteLine("loglar dinleniyor");
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Thread.Sleep(1500);
+
+                Console.WriteLine("Gelen mesaj: " + message);
+
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+
+            Console.ReadLine();
         }
+
     }
 }
